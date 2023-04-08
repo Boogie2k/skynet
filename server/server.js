a835e468e03816a5f7479c5ffe90987d86cea811
Boogie2k <oghosabenjamin@gmail.com>
2023-04-08 21:54:46 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.get("/", async (req, res) => {
 app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
-
+ 
     const response = openai.Completion.create(
       (model = "text-davinci-003"),
       (prompt = `${prompt}`),
